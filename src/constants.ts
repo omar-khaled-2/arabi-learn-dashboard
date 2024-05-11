6b3795bbbb850969f61bdd30d8076a1f46b7acb6
Omar Abdal-salam <sphinx@192.168.1.2>
2024-05-11 14:20:45 +0300

    test

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 
-import dotenv from "dotenv";
 
-dotenv.config();
+import { loadEnvConfig } from '@next/env'
 
+const projectDir = process.cwd()
+loadEnvConfig(projectDir)
 
-console.log(process.env.MONGODB_URL)
 export const MONGODB_URL = process.env.MONGODB_URL!
 export const BUCKET_NAME = process.env.BUCKET_NAME!;
 export const IS_DEV = process.env.NODE_ENV === "development";
