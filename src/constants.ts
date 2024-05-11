818b348f197192951d4c131ddb9dccc2fd07bf28
Omar Abdal-salam <sphinx@192.168.1.2>
2024-05-11 14:12:05 +0300

    deployment

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+
+console.log(process.env.MONGODB_URL)
 export const MONGODB_URL = process.env.MONGODB_URL!
 export const BUCKET_NAME = process.env.BUCKET_NAME!;
 export const IS_DEV = process.env.NODE_ENV === "development";
